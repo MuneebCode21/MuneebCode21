49c69dd99f4d0377fb6d39c68ce14e894efa02ba
Muneeb Rehman <muneeb.ha.456@gmail.com>
2025-09-03 17:00:10 +0500

    updated files

---
+++ first.js
@@ -0,0 +1,8 @@
+console.log('Hello');
+console.log('NEEB IS THE GREAT MAN');
+
+window.alert('STFU U LOOSER');
+
+document.getElementById("Neeb").textContent="DAFUQ";
+document.getElementById("Cheeb").textContent="YOOOO";
+
