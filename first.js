31baa89cd16950e747b90ad8b174569e90dbb233
Muneeb Rehman <muneeb.ha.456@gmail.com>
2025-09-10 21:01:55 +0500

    updated files

--- first.js
+++ first.js
@@ -1,8 +1,94 @@
-console.log('Hello');
-console.log('NEEB IS THE GREAT MAN');
+let string='Hello'
+let boolean=true
+let a=20;
+let b =30;
+let gpa=2.1;
+console.log(string [0]);
+console.log(boolean);
+console.log(`Your GPA is ${gpa} man`);
+console.log(a+b);
+console.log(`NEEB IS THE GREAT MAN`.length);
 
-window.alert('STFU U LOOSER');
+let permisson;
+permisson=window.prompt("WILL YOU GO WITH ME?")
+console.log(permisson);
+window.alert("STFU");
 
-document.getElementById("Neeb").textContent="DAFUQ";
+document.getElementById("Neeb").textContent=`Cuss word is: ${"FUCKITRADICTON"}`;
 document.getElementById("Cheeb").textContent="YOOOO";
+
+let userName;
+
+document.getElementById("mySubmit").onclick=function(){
+    userName = document.getElementById("myName").value;
+    document.getElementById("LOL").textContent = `Nhi aye mujhe: ${userName}`;
+    console.log(userName);
+}
+
+let password;
+let word;
+
+document.getElementById("Submit").onclick=function(){
+    document.getElementById("myValue").value;
+    password=Number(password);
+    word=password+2;
+    document.getElementById("myH3").textContent=word;
+}
+
+let nigga= window.prompt("How old are you?");
+
+nigga += 2;
+nigga=Number(nigga);
+console.log(nigga, typeof nigga)
+
+
+let userEmail="Neeb123@gmail.com";
+let age = 69;
+age=age+25;
+console.log(userEmail);
+console.log(age);
+
+let Name ="Neeb";
+console.log(typeof Name);
+let USD=1;
+PAK=USD * 270;
+console.log(PAK)
+const color='BLACK';
+console.log(`Your skin color is ${color}`);
+
+let students = 30;
+
+students +=2;
+students -=2;
+students *=2;
+students /=2;
+
+console.log(`After +=2 : ${students}`);
+console.log("After -=2 :", students);  
+console.log("After *=2 :", students);  
+console.log("After /=2 :", students);  
+
+
+let extrastudents=students+2;
+let lowstudents=students-2;
+let fatstudents=students*2;
+let stickstudents=students/2;
+
+console.log(extrastudents+" "+lowstudents+ " " +fatstudents+" "+ stickstudents)
+
+console.log(`extra students are: ${extrastudents}, less students are : ${lowstudents}`)
+
+console.log(fatstudents,stickstudents);
+
+let x=2;
+let y=true;
+let z="NEEEBBBBB";
+
+x=Number(x);
+y=Boolean(y);
+z=String(z);
+
+console.log(x, typeof x);
+console.log(y, typeof y);
+console.log(z, typeof z);
 
