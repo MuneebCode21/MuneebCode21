2dd21b3207304b1f492f71ba07f277e73898ceeb
Muneeb Rehman <muneeb.ha.456@gmail.com>
2025-09-11 19:23:15 +0500

    new changes

---
+++ newjavascript.js
@@ -0,0 +1,24 @@
+
+let cheeb = "Nikama";
+let neeb = "goodboy";
+let string="nigga scremeaing"
+let age=22;
+age +=2;
+
+cheeb=Boolean(cheeb);
+console.log(`Haseeb is: ${cheeb}`);
+console.log(neeb,age);
+
+window.prompt("ARE YOU Ok?");
+window.alert(string);
+
+
+document.getElementById("myH1").textContent="YOOOOOOOOOOOOOO";
+
+document.getElementById("butt").onclick = function() {
+    document.getElementById("new").value;
+    document.getElementById("Yalla").textContent = "Yayyyyyy";
+    console.log("Its Working");
+}
+
+
